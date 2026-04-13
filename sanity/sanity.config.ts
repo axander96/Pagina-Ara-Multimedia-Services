fdbca2dc67ca953f6ff2c9373e796921d6c1e493
Alexander Rodríguez <140338044+axander96@users.noreply.github.com>
2026-04-13 18:52:08 -0400

    refactor(sanity): consolidate project structure, remove subfolder

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
-import { schemaTypes } from './schemas'
+import { schemaTypes } from './schemaTypes'
 import { structure } from './sanity/structure'
 
 export default defineConfig({
   name: 'default',
   title: 'ARA Multimedia CMS',
 
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'your-project-id',
+  projectId: 'ddona00k',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
 
   plugins: [
