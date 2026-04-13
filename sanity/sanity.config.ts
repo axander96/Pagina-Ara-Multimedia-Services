d6c2580bae26a14f02198f920adb185cb22c15dc
Alexander Rodríguez <140338044+axander96@users.noreply.github.com>
2026-04-13 16:21:21 -0400

    Sync: Add structure config to main sanity folder

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
+import { structure } from './sanity/structure'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +11,10 @@ export default defineConfig({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'your-project-id',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool({structure}),
+    visionTool()
+  ],
 
   schema: {
     types: schemaTypes,
