7a8c7eabe8c7dbf58f0ee95c8b02a905bf1f45fe
Alexander Rodríguez <140338044+axander96@users.noreply.github.com>
2026-04-14 21:38:27 -0400

    debug: query trae todo el documento de siteConfig

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,9 +1,4 @@
-export const siteConfigQuery = `*[_type == "siteConfig"][0] {
-  ...,
-  logo {
-    asset->
-  }
-}`
+export const siteConfigQuery = `*[_type == "siteConfig"][0]`
 
 export const heroQuery = `*[_type == "hero"][0]`
 
